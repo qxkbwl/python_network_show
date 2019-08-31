d815b311a4e2075d44cbb172bea0abb5b81591a9
qxkbwl <qxkbwl@126.com>
2019-08-31 22:41:11 +0800

    删除错误版本exe

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 python弄的一个查看网速速率，有发送和接受2种
 
 DIST目录有EXE文件，可直接运行    
+
+
+使用pyinstaller打包
+
+命令：pyinstaller -F -W network_python.py
+#解释：
+    -F 打包成一个exe文件
+    -w 不显示命令面板
+
--- network_python.py
+++ network_python.py
@@ -6,41 +6,42 @@ import tkinter as tk
 
 
 network_list = [i for i in psutil.net_io_counters()]#获取网络数据
-up_send = network_list[2]#发送数据包 单位字节
-up_recv = network_list[3]#接受数据包 单位字节
+up_send = network_list[0]#发送数据包 单位字节
+up_recv = network_list[1]#接受数据包 单位字节
 
 def disponse_network():
-    global up_send
-    global up_recv
+    global up_send#本次发送速度结果
+    global up_recv#本次接受速度结果
+
+    sleep(1)#需要在采集这里等待一秒，正好一秒内速率（在其他地方延迟一秒都会导致速率不正确，测试过）
 
     network_list = [i for i in psutil.net_io_counters()]#获取网络数据
-    network_send = network_list[2]#发送数据包 单位字节
-    network_recv = network_list[3]#接受数据包 单位字节
+    network_send = network_list[0]#发送数据包 单位字节
+    network_recv = network_list[1]#接受数据包 单位字节
     #现在的总数据字节减去上次的数据字节，等于现在的网速！
-    network_send -= up_send
-    network_recv -= up_recv
-    #判断是否大于1048576,则单位为mb,默认kb
-    if network_send >= 1048576:
+    temporary_send = network_send - up_send
+    temporary_recv = network_recv - up_recv
+    #判断是否大于1048576,则单位为mb,默认b
+    if temporary_send >= 1048576:
         send_unit = "MB"
-    elif network_recv >= 1025:# kb
+        send = (temporary_send // 1024) // 1024
+    elif temporary_send >= 1024:# kb
         send_unit = "KB"
+        send = temporary_send // 1024
     else:
-        send_unit = "b"
+        send = temporary_send
+        send_unit = "B"
 
-    if network_recv >= 1048576:# mb
+    if temporary_recv >= 1048576:# mb
+        recv = (temporary_recv // 1024) //1024
         recv_unit = "MB"
-    elif network_recv >= 1025:# kb
+    elif temporary_recv >= 1024:# kb
         recv_unit = "KB"
+        recv = temporary_recv // 1024
     else:
-        recv_unit = "b"
+        recv_unit = "B"
+        recv = temporary_recv
 
-    send = round(network_send/1024, 3)
-    recv = round(network_recv/1024, 3)
-
-    # print("发送：%s"%send)
-    # print("接收：%s"%recv)
-    sleep(1)
-    # print('开始下一轮')
     #最后操作完成赋值这次的数据字节，下次再计算
     up_send = network_send
     up_recv = network_recv
@@ -53,6 +54,7 @@ def hit_me():
         return False
     except:
         return True
+
     # print("发送：%s/n接收：%s"%(send, recv))
     
 
@@ -88,5 +90,6 @@ while True:
         break
 
     error_status = hit_me()
+    
         
 
